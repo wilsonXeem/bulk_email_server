aed71960edd1837e767bac9af720b2186756cdeb
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2025-03-05 18:51:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post("/send-bulk-email", async (req, res) => {
 });
 
 app.get("/", (req, res, next) =>
-  res.status(200).json({ message: "Hello World" })
+  res.send("Hello World");
 );
 
 const PORT = process.env.PORT || 5000;
